eeffe84b2511a78e12f7e1015539ec982b4b2b82
COCONUTmkII <coconutmkii@gmail.com>
2022-05-18 00:11:40 +0300

    Added service, repository, model, controller layers

---
+++ kotlinpath/demojavakotlin/src/main/kotlin/by/itechart/demojavakotlin/controller/MovieController.kt
@@ -0,0 +1,27 @@
+package by.itechart.demojavakotlin.controller
+
+import by.itechart.demojavakotlin.entity.MovieEntity
+import by.itechart.demojavakotlin.model.MovieRequest
+import by.itechart.demojavakotlin.service.MovieService
+import org.springframework.http.HttpStatus.NO_CONTENT
+import org.springframework.http.HttpStatus.OK
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/movies")
+class MovieController(private val movieService: MovieService) {
+    @GetMapping("/all")
+    fun getMovies(): ResponseEntity<List<MovieEntity>> = ResponseEntity(movieService.getAll(), OK)
+
+    @PostMapping
+    fun addMovie(requestMovie: MovieRequest): ResponseEntity<MovieEntity> =
+            ResponseEntity(movieService.addMovie(requestMovie), OK)
+
+    @DeleteMapping("/{id}")
+    fun deleteMovie(@PathVariable id: UUID): ResponseEntity<Void> {
+        movieService.deleteMovie(id)
+        return ResponseEntity(NO_CONTENT)
+    }
+}
---
+++ kotlinpath/demojavakotlin/src/main/kotlin/by/itechart/demojavakotlin/service/impl/TicketServiceImpl.kt
@@ -0,0 +1,26 @@
+package by.itechart.demojavakotlin.service.impl
+
+import by.itechart.demojavakotlin.entity.TicketEntity
+import by.itechart.demojavakotlin.model.TicketRequest
+import by.itechart.demojavakotlin.repository.TicketRepository
+import by.itechart.demojavakotlin.service.MovieService
+import by.itechart.demojavakotlin.service.TicketService
+import org.springframework.stereotype.Service
+
+@Service
+class TicketServiceImpl(
+        private val ticketRepository: TicketRepository,
+        private val movieService: MovieService) : TicketService {
+
+    override fun buyTicket(quantity: Int): List<TicketEntity?>? = ticketRepository.buyTicket(quantity)
+
+    override fun addTicketsToMovie(ticketsRequest: TicketRequest): TicketEntity = ticketRepository.saveAndFlush(
+            TicketEntity(quantity = ticketsRequest.quantity, price = ticketsRequest.price)
+    )
+
+    override fun removeTicketsByMovieName(movieName: String?) {
+        movieService.getMovieByName(movieName)?.let {
+            ticketRepository.deleteByMovieId(it)
+        }
+    }
+}
